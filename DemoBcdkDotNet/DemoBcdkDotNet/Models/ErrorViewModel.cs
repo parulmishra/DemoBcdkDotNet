f924c3d1e01ffb05f0e4fd0f968ada027b0a0a90
Parul Mishra Dubedy <parul.mishra78@gmail.com>
2019-11-28 17:07:27 -0800

    First checkin for the Demo BCDK on .NET project

---
+++ DemoBcdkDotNet/DemoBcdkDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoBcdkDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
